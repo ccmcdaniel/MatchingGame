60852952fccbfff92d830f01b0c39f385c200595
ccmcdaniel <93947281+ccmcdaniel@users.noreply.github.com>
2021-11-16 11:15:06 -0600

    complete game

--- MatchingGame/Form1.cs
+++ MatchingGame/Form1.cs
@@ -7,12 +7,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Media;
 
 namespace MatchingGame
 {
     public partial class Form1 : Form
     {
+        Label firstClicked = null;
+        Label secondClicked = null;
+
+        bool allowClicking = true;
+
         Random random = new Random();
+
+        SoundPlayer pop;
 
         List<string> icons = new List<string>()
         {
@@ -24,6 +32,8 @@ namespace MatchingGame
         {
             InitializeComponent();
             initializeIcons();
+
+            pop = new SoundPlayer(MatchingGame.Properties.Resources.popSound);
         }
 
         private void initializeIcons()
@@ -45,9 +55,78 @@ namespace MatchingGame
 
         private void label1_Click(object sender, EventArgs e)
         {
+            Form frm = new frmNewForm();
+
+            frm.Show();
+            frm.Hide();
+
             Label icon = sender as Label;
 
+            if (allowClicking == false)
+                return;
+
+            if (icon.ForeColor == Color.Black)
+                return;
+
+            if (firstClicked == null)
+            {
+                firstClicked = icon;
+            }
+            else if(secondClicked == null && firstClicked != icon)
+            {
+                secondClicked = icon;
+
+                if(firstClicked.Text == secondClicked.Text)
+                {
+                    firstClicked = null;
+                    secondClicked = null;
+                }
+                else
+                {
+                    //Didn't match
+                    hideMismatches.Start();
+                    allowClicking = false;
+                }
+            }
+
+
             icon.ForeColor = Color.Black;
+
+            pop.Play();
+
+            if (checkWin() == true)
+            {
+                if(MessageBox.Show("You did it!!!", "Congrats") == DialogResult.OK)
+                {
+                    Application.Exit();
+                }
+            }
+        }
+
+        private bool checkWin()
+        {
+            foreach (Control c in tableLayoutPanel1.Controls)
+            {
+                Label icon = c as Label;
+
+                if (icon.ForeColor == icon.BackColor)
+                    return false;
+            }
+
+            return true;
+        }
+
+        private void hideMismatches_Tick(object sender, EventArgs e)
+        {
+            hideMismatches.Stop();
+
+            firstClicked.ForeColor = firstClicked.BackColor;
+            secondClicked.ForeColor = secondClicked.BackColor;
+
+            firstClicked = null;
+            secondClicked = null;
+
+            allowClicking = true;
         }
     }
 }
